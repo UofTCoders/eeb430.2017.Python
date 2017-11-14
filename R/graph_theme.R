e147d5afa32c8ba3106d026dfb69392508b5d37d
Ben <ben.downer.bartholomew@mail.utoronto.ca>
2017-11-14 15:13:00 -0500

    Team's graph theme

---
+++ R/graph_theme.R
@@ -0,0 +1,11 @@
+team_theme <- function() {list(
+  
+  theme(axis.line = element_line(color = "black"),
+        text = element_text(size = 8, family = "Times"),
+        panel.background = element_rect(fill = 'white', colour = 'black'),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        plot.title = element_text(colour =  "black", size = 14, hjust = 0.5),
+        legend.text = element_text(size = 12, family = "Times")),
+  scale_colour_colorblind())
+}
